08e7d482d3ccba1a992cb1b50c1fd304566b029b
Katherina Kaliaha <mellinda077@mail.ru>
2022-12-11 14:09:19 +0300

    feat: add cd, cp and rn functions

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ import * as readline from "node:readline/promises"
 import { fileURLToPath } from "url"
 import path from "path"
 import os from "node:os"
-import { readdir } from "node:fs/promises"
+import { readdir, writeFile, rename } from "node:fs/promises"
+import { createReadStream, createWriteStream } from "node:fs"
 
 const __filename = fileURLToPath(import.meta.url)
 
@@ -15,9 +16,9 @@ const userData = process.argv.slice(2)
 const prefix = "--username="
 const name = userData[0].slice(prefix.length)
 
-const directory = (path) => {
+const directory = async (path) => {
     process.stdout.write(`You are currently in ${path} ${os.EOL}`)
-    // console.log(os.platform(), os.homedir())
+    // console.log(process.chdir(os.homedir()), os.homedir(), process.cwd())
 }
 
 function startApp() {
@@ -29,14 +30,18 @@ function startApp() {
             throw new Error("Error: Invalid data")
         }
         process.stdout.write(`Welcome to the File Manager, ${name}! ${os.EOL}`)
-        currentPath = process.cwd()
+        // process.chdir(os.homedir())
+        currentPath = os.homedir()
+        // directory(process.cwd())
         directory(currentPath)
     } catch (err) {
         console.error(err.message)
     }
 }
-rl.on("line", (input) => {
-    switch (input) {
+rl.on("line", async (input) => {
+    const consoleInput = input.split(" ")
+
+    switch (consoleInput[0]) {
         case ".exit": {
             rl.close()
             break
@@ -47,8 +52,59 @@ rl.on("line", (input) => {
             break
         }
         case "ls": {
-            ls()
-            directory(currentPath)
+            await ls()
+            await directory(currentPath)
+            break
+        }
+        case "cat": {
+            try {
+                if (consoleInput.length === 2) {
+                    await cat(consoleInput.slice(1).join(" "))
+                } else {
+                    console.log(`Error`)
+                }
+            } catch {
+                console.log("Error")
+            } finally {
+            }
+            break
+        }
+
+        case "add": {
+            if (consoleInput.length === 2) {
+                await add(consoleInput.slice(1).join(" "))
+                await directory(currentPath)
+            } else {
+                console.log(`Error`)
+            }
+            break
+        }
+        case "cd": {
+            if (consoleInput.length === 2) {
+                cd(consoleInput.slice(1).join(" "))
+            } else {
+                console.log(`Error`)
+                directory(currentPath)
+            }
+            break
+        }
+        case "rn": {
+            console.log(consoleInput)
+            if (consoleInput.length === 3) {
+                await rn(consoleInput.slice(1))
+                await directory(currentPath)
+            } else {
+                console.log(`Error`)
+            }
+            break
+        }
+        case "cp": {
+            if (consoleInput.length === 3) {
+                cp(consoleInput.slice(1))
+                // await directory(currentPath)
+            } else {
+                console.log(`Error`)
+            }
             break
         }
         default: {
@@ -56,7 +112,7 @@ rl.on("line", (input) => {
         }
     }
 })
-rl.on("close", (input) => {
+rl.on("close", () => {
     console.log(`Thank you for using File Manager, ${name}, goodbye! ${os.EOL}`)
 })
 
@@ -91,5 +147,78 @@ async function ls() {
     ]
 
     console.table(list)
+}
+
+async function cat(fileToRead) {
+    const readTextFile = createReadStream(path.resolve(fileToRead))
+    readTextFile.on("data", function (chunk) {
+        console.log(chunk.toString())
+    })
+    readTextFile.on("end", () => directory(currentPath))
+    readTextFile.on("error", () => console.log("Error"))
+}
+
+async function add(fileToWrite) {
+    await writeFile(`${currentPath}${path.sep}${fileToWrite}`, "")
+}
+
+async function cd(file) {
+    try {
+        if (file === "..") {
+            up()
+            directory(currentPath)
+        } else {
+            const filePath = path.resolve(currentPath, file)
+            // process.chdir(filePath)
+            // console.log(process.cwd())
+            if (path.isAbsolute(file)) {
+                const files = await readdir(file, { withFileTypes: true })
+                if (files) {
+                    currentPath = file
+                    directory(currentPath)
+                }
+            } else {
+                const files = await readdir(filePath, { withFileTypes: true })
+                if (files) {
+                    currentPath = filePath
+                    directory(currentPath)
+                }
+            }
+        }
+    } catch {
+        console.log("Error")
+        directory(currentPath)
+    }
+}
+
+async function rn(names) {
+    try {
+        const [oldName, newName] = names
+        await rename(`${currentPath}/${oldName}`, `${currentPath}/${newName}`)
+    } catch {
+        console.log("Error in rn!")
+    }
+}
+
+async function cp(names) {
+    try {
+        const [fileToRead, directoryToWrite] = names
+        const pathToDirectory = path.normalize(
+            `${currentPath}${path.sep}${directoryToWrite}`
+        )
+        const files = await readdir(pathToDirectory, { withFileTypes: true })
+        if (files) {
+            const readTextFile = createReadStream(path.resolve(fileToRead))
+            await writeFile(`${pathToDirectory}${path.sep}${fileToRead}`, "")
+            const writeTextFile = createWriteStream(
+                `${pathToDirectory}${path.sep}${fileToRead}`
+            )
+            readTextFile.pipe(writeTextFile)
+            await directory(currentPath)
+        }
+    } catch {
+        console.log("Error in cp!")
+        await directory(currentPath)
+    }
 }
 startApp()
